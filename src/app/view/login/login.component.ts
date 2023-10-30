ea343d143bf994a53f3a79a3dd19c872dfdcf011
lethanhtung1303 <letung844@gmail.com>
2023-10-30 23:12:00 +0700

    validator login

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {forbiddenValueValidator} from "../../helper/validators";
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  user = {inputEmail: '', inputPassword: ''};
+
+  loginForm!: FormGroup;
+
+  constructor() {
+  }
+
+  get inputEmail() {
+    return this.loginForm.get('inputEmail')!;
+  }
+
+  get inputPassword() {
+    return this.loginForm.get('inputPassword')!;
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+        inputEmail: new FormControl(this.user.inputEmail, [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        inputPassword: new FormControl(this.user.inputPassword, [
+          Validators.required,
+          Validators.minLength(6),
+          forbiddenValueValidator(/^123456$/)
+        ]),
+      },
+    );
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
+
 }
