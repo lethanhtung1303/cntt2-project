91d553adf6abcf513605d9b8ad7b6edfaf275609
lethanhtung1303 <letung844@gmail.com>
2024-01-20 19:25:41 +0700

    Add Teaching Diary and update Home page

--- src/app/view/Lecturers/lecturers-detail/tab/lecturer-teaching-history/lecturer-teaching-history.component.ts
+++ src/app/view/Lecturers/lecturers-detail/tab/lecturer-teaching-history/lecturer-teaching-history.component.ts
@@ -1,19 +1,31 @@
-import {Component, Input, OnChanges} from '@angular/core';
-import {SelectItemGroup} from "primeng/api";
-import {SemesterResponse, SemesterService} from "../../../../../service/semester.service";
-import {LecturerTeachingHistory} from "../../../../../domain/lecturer";
-import {saveAsExcelFile} from "../../../../../helper/excel-helper";
-import {Table} from "primeng/table";
+import { Component, Input, OnChanges } from '@angular/core';
+import {
+  ConfirmationService,
+  ConfirmEventType,
+  MessageService,
+  SelectItemGroup,
+} from 'primeng/api';
+import {
+  SemesterResponse,
+  SemesterService,
+} from '../../../../../service/semester.service';
+import { LecturerTeachingHistory } from '../../../../../domain/lecturer';
+import { saveAsExcelFile } from '../../../../../helper/excel-helper';
+import { Table } from 'primeng/table';
 import {
   LecturerTeachingHistoryService,
-  TeachingHistoryResponse
-} from "../../../../../service/lecturer-teaching-history.service";
-import {getCurrentSemester} from "../../../../../helper/semesters";
+  TeachingHistoryResponse,
+} from '../../../../../service/lecturer-teaching-history.service';
+import { getCurrentSemester } from '../../../../../helper/semesters';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserHelper } from '../../../../../helper/user-helper';
+import { PreviousRouteService } from '../../../../../helper/previous-route.service';
 
 @Component({
   selector: 'app-lecturer-teaching-history',
   templateUrl: './lecturer-teaching-history.component.html',
-  styleUrls: ['./lecturer-teaching-history.component.css']
+  styleUrls: ['./lecturer-teaching-history.component.css'],
+  providers: [ConfirmationService, MessageService],
 })
 export class LecturerTeachingHistoryComponent implements OnChanges {
   @Input() lecturerId?: number;
@@ -23,45 +35,124 @@ export class LecturerTeachingHistoryComponent implements OnChanges {
 
   teachingHistory: LecturerTeachingHistory[] = [];
 
-  constructor(private semesterService: SemesterService, private lecturerTeachingHistoryService: LecturerTeachingHistoryService) {
+  userLogin: string;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private semesterService: SemesterService,
+    private lecturerTeachingHistoryService: LecturerTeachingHistoryService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private userHelper: UserHelper,
+    private previousRouteService: PreviousRouteService,
+  ) {
+    this.previousRouteService.setPreviousUrl(this.router.url);
+
+    this.userLogin = userHelper.getUserLogin();
+
     this.semesterService.getAllSemester().subscribe({
       next: (data: SemesterResponse) => {
         this.groupedSemesters = data.results.semesters;
-        this.selectedSemester = getCurrentSemester(this.groupedSemesters)?.value
+        this.selectedSemester = getCurrentSemester(
+          this.groupedSemesters,
+        )?.value;
         if (this.selectedSemester && this.lecturerId) {
-          this.getLecturerTeachingHistory(this.selectedSemester, this.lecturerId)
+          this.getLecturerTeachingHistory(
+            this.selectedSemester,
+            this.lecturerId,
+          );
         }
       },
       error: (error) => {
-        console.log(error)
-      }
+        console.log(error);
+      },
     });
   }
 
   ngOnChanges(): void {
     if (this.selectedSemester && this.lecturerId) {
-      this.getLecturerTeachingHistory(this.selectedSemester, this.lecturerId)
+      this.getLecturerTeachingHistory(this.selectedSemester, this.lecturerId);
     }
   }
 
   onChangeSemester() {
     if (this.selectedSemester && this.lecturerId) {
-      this.getLecturerTeachingHistory(this.selectedSemester, this.lecturerId)
+      this.getLecturerTeachingHistory(this.selectedSemester, this.lecturerId);
     }
   }
 
   getLecturerTeachingHistory(semester: number, lecturerId: number) {
-    this.lecturerTeachingHistoryService.getTeachingHistory({
-      semester,
-      lecturerId
-    }).subscribe({
-      next: (data: TeachingHistoryResponse) => {
-        this.teachingHistory = data.results.teachingHistory;
-        this.loading = false;
+    this.lecturerTeachingHistoryService
+      .getTeachingHistory({
+        semester,
+        lecturerId,
+      })
+      .subscribe({
+        next: (data: TeachingHistoryResponse) => {
+          this.teachingHistory = data.results.teachingHistory;
+          this.loading = false;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  confirmDelete(historyId: number) {
+    this.confirmationService.confirm({
+      key: historyId.toString(),
+      accept: () => {
+        this.lecturerTeachingHistoryService
+          .deleteHistory({
+            historyId: historyId.toString(),
+            deleteBy: this.userLogin,
+          })
+          .subscribe({
+            next: () => {
+              this.messageService.add({
+                key: historyId.toString(),
+                severity: 'error',
+                summary: 'Đã xoá',
+                detail: `Bạn đã xoá Lịch sử Giảng dạy: ${historyId}`,
+              });
+              this.router
+                .navigate([`/lecturers/${this.lecturerId}`], {
+                  relativeTo: this.activatedRoute,
+                  queryParams: { tab: 'teaching-history' },
+                  queryParamsHandling: 'merge',
+                })
+                .then(() => {
+                  setTimeout(() => {
+                    window.location.reload();
+                  }, 1000);
+                });
+            },
+            error: (error) => {
+              console.log(error);
+            },
+          });
       },
-      error: (error) => {
-        console.log(error)
-      }
+      reject: (type: ConfirmEventType) => {
+        switch (type) {
+          case ConfirmEventType.REJECT:
+            this.messageService.add({
+              key: historyId.toString(),
+              severity: 'info',
+              summary: 'Đã huỷ',
+              detail: 'You have cancelled',
+            });
+            break;
+          case ConfirmEventType.CANCEL:
+            this.messageService.add({
+              key: historyId.toString(),
+              severity: 'info',
+              summary: 'Đã huỷ',
+              detail: 'You have cancelled',
+            });
+            break;
+        }
+      },
     });
   }
 
@@ -72,7 +163,7 @@ export class LecturerTeachingHistoryComponent implements OnChanges {
   exportExcel() {
     import('xlsx').then((xlsx) => {
       const worksheet = xlsx.utils.json_to_sheet(this.teachingHistory);
-      const workbook = {Sheets: {data: worksheet}, SheetNames: ['data']};
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
       const excelBuffer: any = xlsx.write(workbook, {
         bookType: 'xlsx',
         type: 'array',
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,47 +1,82 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  LecturerResponse,
+  LecturerService,
+} from '../../service/lecturer.service';
+import { SubjectResponse, SubjectService } from '../../service/subject.service';
+import { Lecturer } from '../../domain/lecturer';
+import { Subject } from '../../domain/subject';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
   images: any[] | undefined;
 
   responsiveOptions: any[] = [
     {
       breakpoint: '1024px',
-      numVisible: 5
+      numVisible: 5,
     },
     {
       breakpoint: '768px',
-      numVisible: 3
+      numVisible: 3,
     },
     {
       breakpoint: '560px',
-      numVisible: 1
-    }
+      numVisible: 1,
+    },
   ];
+  totalLecturers: number = 0;
+  totalSubjects: number = 0;
+  lecturers: Lecturer[] = [];
+  subjects: Subject[] = [];
 
-  constructor() {
-    this.getImages().then((images) => (this.images = images))
+  constructor(
+    private lecturerService: LecturerService,
+    private subjectService: SubjectService,
+  ) {
+    this.getImages().then((images) => (this.images = images));
+
+    this.lecturerService.getAllLecturer().subscribe({
+      next: (data: LecturerResponse) => {
+        this.totalLecturers = data.results.resultsTotalCount;
+        this.lecturers = data.results.lecturers;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+
+    this.subjectService.getAllSubject().subscribe({
+      next: (data: SubjectResponse) => {
+        this.totalSubjects = data.results.resultsTotalCount;
+        this.subjects = data.results.subjects;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
-
 
   getData() {
     return [
       {
         itemImageSrc: 'https://stdportal.tdtu.edu.vn/images/sliders/T7-2.jpg',
-        thumbnailImageSrc: 'https://stdportal.tdtu.edu.vn/images/sliders/T7-2.jpg',
+        thumbnailImageSrc:
+          'https://stdportal.tdtu.edu.vn/images/sliders/T7-2.jpg',
         alt: 'Description for Image 1',
-        title: 'Title 1'
+        title: 'Title 1',
       },
       {
-        itemImageSrc: 'https://stdportal.tdtu.edu.vn/images/sliders/Th%C6%B0%20Vi%E1%BB%87n.jpg',
-        thumbnailImageSrc: 'https://stdportal.tdtu.edu.vn/images/sliders/Th%C6%B0%20Vi%E1%BB%87n.jpg',
+        itemImageSrc:
+          'https://stdportal.tdtu.edu.vn/images/sliders/Th%C6%B0%20Vi%E1%BB%87n.jpg',
+        thumbnailImageSrc:
+          'https://stdportal.tdtu.edu.vn/images/sliders/Th%C6%B0%20Vi%E1%BB%87n.jpg',
         alt: 'Description for Image 2',
-        title: 'Title 2'
+        title: 'Title 2',
       },
     ];
   }
@@ -50,4 +85,25 @@ export class HomeComponent {
     return Promise.resolve(this.getData());
   }
 
+  getContractualLecturer() {
+    return this.lecturers.filter(
+      (lecturer) => lecturer.classificationLecturers.maLoai === 1,
+    ).length;
+  }
+
+  getVisitingLecturer() {
+    return this.lecturers.filter(
+      (lecturer) => lecturer.classificationLecturers.maLoai !== 1,
+    ).length;
+  }
+
+  getTheorySubjects() {
+    return this.subjects.filter((subject) => subject.phanLoai === 'Lý thuyết')
+      .length;
+  }
+
+  getPracticeSubjects() {
+    return this.subjects.filter((subject) => subject.phanLoai === 'Thực hành')
+      .length;
+  }
 }
